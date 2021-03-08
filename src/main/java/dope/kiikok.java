4f3a0576b5ff82cc70e94c8ce33c6a006a56b198
karl-chanel <karl_chanel@outlook.com>
2021-03-08 22:54:16 +0800

    changing

---
+++ src/main/java/dope/kiikok.java
@@ -0,0 +1,12 @@
+package dope;
+
+/**
+ * @author 86176
+ * @package dope
+ * @date 2021/3/8 22:51
+ */
+public class kiikok {
+    public static void main(String[] args) {
+        System.out.println("dcefc");
+    }
+}
